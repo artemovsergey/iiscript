6c53457c5547b49601220ae75f111b2e36003c5a
artemovsergey <artik3314@gmail.com>
2020-04-23 23:43:14 +0300

    .

--- iiscript.rb
+++ iiscript.rb
@@ -1,34 +1,23 @@
 require 'sinatra'
-#require 'docx'
-#require 'date'
-#require 'mail'
+require 'docx'
+require 'date'
+require 'mail'
 
 get '/' do
-  erb :index
-end
 
-=begin
-date = Time.new.day.to_s + '.0' + Time.new.month.to_s + '.' + Time.new.year.to_s
+   date = Time.new.day.to_s + '.0' + Time.new.month.to_s + '.' + Time.new.year.to_s
+   doc = Docx::Document.open('template.docx')
 
-doc = Docx::Document.open('template.docx')
-
-
-doc.paragraphs.each do |p|
+   doc.paragraphs.each do |p|
     p.each_text_run do |tr|
       tr.substitute('date',"#{date}")
     end
   end
- 
-doc.save("#{Time.new.day.to_s}.docx")
+
+  doc.save("#{Time.new.day.to_s}.docx")
 
 
-=begin
-# Retrieve and display paragraphs
-doc.paragraphs.each do |p|
-  puts p
-end
-
-Mail.defaults do
+  Mail.defaults do
     delivery_method :smtp, {
       address: 'smtp.gmail.com',
       port: 587,
@@ -40,13 +29,21 @@ Mail.defaults do
     }
   end
 
+	Mail.deliver do
+	  from      "artik3314@gmail.com"
+	  to        "artik3314@gmail.com"
+	  subject   ""
+	  body      ""
+	  add_file  "#{Time.new.day.to_s}.docx"
+	end
 
-Mail.deliver do
-  from      "artik3314@gmail.com"
-  to        "artik3314@gmail.com"
-  subject   ""
-  body      ""
-  add_file  "#{Time.new.day.to_s}.docx"
+
+
+
+  erb :index
+
 end
 
-=end
+
+
+
